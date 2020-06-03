5b0d53f9fa82a49498ebbae3fa03608133c398ad
Jonathan Haist <jonathan.haist@t-online.de>
2020-06-03 18:06:00 +0200

    add Mileage

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace dhbw_vs_aspdotnetcoretutorial.Models
 {
@@ -10,7 +7,7 @@ namespace dhbw_vs_aspdotnetcoretutorial.Models
         public int Id { get; set; }
         public string Manufacturer { get; set; }
         public string Model { get; set; }
-
+        public int Mileage { get; set; }
         public double PurchasePrice { get; set; }
         public DateTime FirstRegistration { get; set;}
     }
