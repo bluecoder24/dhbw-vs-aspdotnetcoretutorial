e4e7089b655e1bc0d8832c7e34b590f57add71ba
Jonathan Haist <jonathan.haist@t-online.de>
2020-06-03 18:00:48 +0200

    remove WeatherForecast.cs; add Models with Car.cs

---
+++ Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dhbw_vs_aspdotnetcoretutorial.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+
+        public double PurchasePrice { get; set; }
+        public DateTime FirstRegistration { get; set;}
+    }
+}
